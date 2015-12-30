6feb6588a121dee17b524453ce8696961655aa41
Maros Korinek <maros.korinek@fundingcircle.com>
2015-12-30 17:44:11 +0000

    User can wrap related forms with tag
    This commit includes change of the core the api for setting tag to `set-log-tag`
    instead of `set-tag`.

--- test/clj_loga/support/logging_helpers.clj
+++ test/clj_loga/support/logging_helpers.clj
@@ -1,13 +1,21 @@
 (ns clj-loga.support.logging-helpers
-  (:require [taoensso.timbre :as timbre]))
+  (:require [cheshire.core :refer [parse-string]]))
 
-(def log-event (atom nil))
+(declare get-log-element)
+
+(def log-events (atom ()))
 
 (defn reset-log-events []
-  (swap! log-event (fn [_] nil)))
+  (swap! log-events (fn [_] ())))
 
 (defn- store-event [event]
-  (swap! log-event (fn [_] event)))
+  (swap! log-events (fn [logged-events] (conj logged-events event))))
+
+(defn latest-log-event []
+  (first @log-events))
+
+(defn earliest-log-event []
+  (last @log-events))
 
 (def atom-appender
   {:appenders
@@ -23,4 +31,11 @@
              formatted-output-str (output-fn data)]
          (store-event formatted-output-str)))}}})
 
+(defn get-log-element [log element]
+  (get (parse-string log) element))
 
+(comment
+  @log-events
+  (store-event "a-event")
+  (reset-log-events)
+  )
